551fea3984d86cdc4ee2d5381050dbfe1e2c8cc4
gio cornejo <cornejog553@gmail.com>
2019-04-07 18:03:23 -0700

    Added listing of menu and list numbers functions along with add numbers to list function

--- Loops/main.cpp
+++ Loops/main.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
-    return 0;
+    
+    int status {1};
+    char action {'P'};
+    vector <int> numberList;
+	while(status == 1){
+        cout << "P - Print Numbers" << endl;
+        cout << "A - Add a Number" << endl;
+        cout << "M - Display mean of the numbers" << endl;
+        cout << "S - Display the smallest number" << endl;
+        cout << "L - Display the largest number" << endl;
+        cout << "Q - Quit" << endl << endl;
+        
+        cout << "Enter your choice: ";
+        
+        cin >> action;
+        
+        switch(action){
+            case 'P':
+            case 'p':
+                if(numberList.size() == 0){
+                    cout << "[] - the list is empty" << endl << endl;
+                }else{
+                    for(unsigned int count {0}; count < numberList.size(); count++){
+                    cout << "[ " << numberList.at(count);
+                } 
+                cout <<" ]" << endl; 
+                }
+            break;
+            case 'A':
+            case 'a':
+            {
+                int addNumber {0};
+                cout << "Enter an integer to add to the list: " << endl;
+                cin >> addNumber;
+                numberList.push_back(addNumber);
+                cout << addNumber << " added" << endl;
+                break; 
+            }
+            
+            case 'M':
+            case 'm':
+            cout << "You chose m" << endl;
+            break;
+            case 'S':
+            case 's':
+            cout << "You chose s" << endl;
+            break;
+            case 'L':
+            case 'l':
+            cout << "You chose l" << endl;
+            break;
+            case 'Q':
+            case 'q':
+            cout << "Goodbye" << endl;
+            status = 0;
+            break;
+            default : 
+            cout << "That is not a valid choice" << endl;
+        }
+    }
 }
